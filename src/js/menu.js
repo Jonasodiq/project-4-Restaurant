e3e6cba9152c05f2f87a40dc0cf87af5ea7c55a9
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2023-09-16 15:22:23 +0200

    Add closeMenu function

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,22 +1,26 @@
 const siteMenu = () => {
+  // Get element from DOM
+  const menuButton = document.querySelector('.menu-button'); // Hämta knappen som öppnar menyn
+  const menu = document.querySelector('.site-menu'); // Hämta menyn
+  const bookButton = document.querySelector('.site-header__button'); // Hämta bokningsknappen
+  const menuItem = document.querySelector('.site-menu__list');
 
-    // Get element from DOM
-    const menuButton = document.querySelector('.menu-button')
-    const menu = document.querySelector('.site-menu')
-    const bookButton = document.querySelector('.site-header__button')
+  // Toggle Menu - Funktion för att växla (visa/dölja) menyn
+  const toggleMenu = () => {
+    menu.classList.toggle('site-menu--visible'); // Lägg till eller ta bort CSS-klassen 'site-menu--visible' på menyn för att visa eller dölja den
+    menuButton.classList.toggle('menu-button--close'); // Lägg till eller ta bort CSS-klassen 'menu-button--close' på knappen för att ändra dess utseende
+    bookButton.classList.toggle('site-header__button--menu-opened'); // Lägg till eller ta bort CSS-klassen 'site-header__button--menu-opened' på bokningsknappen för att ändra dess utseende
+  };
 
-    // Toggle Menu
-    const toggleMenu = () => {
-        menu.classList.toggle('site-menu--visible')
-        menuButton.classList.toggle('menu-button--close')
-        bookButton.classList.toggle('site-header__button--menu-opened')
-    }
+  const closeMenuItem = () => {
+    menu.classList.remove('site-menu--visible');
+  };
 
-    // Create Event
-    menuButton.addEventListener('click', toggleMenu)
+  // Create Event - Skapa en händelselyssnare för att lyssna på klick på menyknappen
+  menuButton.addEventListener('click', toggleMenu);
+  menuItem.addEventListener('click', closeMenuItem);
 
+  console.log('Menu started...');
+};
 
-    console.log('Menu started...')
-}
-
-siteMenu()
+siteMenu(); // Anropa funktionen för att aktivera menyn
